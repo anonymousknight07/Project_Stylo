d8ae360045df75d87a45e675b9c9aa989479e917
Akshat <akshath.centaurus@gmail.com>
2023-04-22 10:18:22 +0530

    first commit

---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,57 @@
+import { motion, AnimatePresence } from 'framer-motion';
+import { useSnapshot } from 'valtio';
+
+import state from '../store';
+import { CustomButton } from '../components';
+import {
+  headContainerAnimation,
+  headContentAnimation,
+  headTextAnimation,
+  slideAnimation
+} from '../config/motion';
+
+const Home = () => {
+  const snap = useSnapshot(state);
+
+  return (
+    <AnimatePresence>
+      {snap.intro && (
+        <motion.section className="home" {...slideAnimation('left')}>
+          <motion.header {...slideAnimation("down")}>
+            <img 
+              src='./threejs.png'
+              alt="logo"
+              className="w-8 h-8 object-contain"
+            />
+          </motion.header>
+
+          <motion.div className="home-content" {...headContainerAnimation}>
+            <motion.div {...headTextAnimation}>
+              <h1 className="head-text">
+                Stylo-<br className="xl:block hidden" />Threads
+              </h1>
+            </motion.div>
+            <motion.div
+              {...headContentAnimation}
+              className="flex flex-col gap-5"
+            >
+              <p className="max-w-md font-normal text-gray-600 text-base">
+              {/* Create your unique and exclusive shirt with our brand-new 3D customization tool. <strong>Unleash your imagination</strong>{" "} and define your own style. */}
+             <strong>Welcome to Stylo-Threads:Your Ultimate Shirt Design Playground !</strong>{" "}Our cutting-edge website lets you unleash your creativity and design your own custom shirts with the help of advanced AI technology. From selecting colors, patterns, and styles to adding personalized text and graphics, creating your dream shirt has never been easier. <strong>Get ready to rock your unique style with Stylo-Threads!</strong>
+              </p>
+
+              <CustomButton 
+                type="filled"
+                title="Customize It"
+                handleClick={() => state.intro = false}
+                customStyles="w-fit px-4 py-2.5 font-bold text-sm"
+              />
+            </motion.div>
+          </motion.div>
+        </motion.section>
+      )}
+    </AnimatePresence>
+  )
+}
+
+export default Home
